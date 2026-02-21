1d13db5128955af7e1b91adbbac1c0ee2b36c6a0
reayd-awsbot <david@awsbot.com>
2026-02-21 15:49:36 +0000

    upats

--- win32_stub/skip_mmintrin.h
+++ win32_stub/skip_mmintrin.h
@@ -2,6 +2,10 @@
  * Force the real mmintrin.h to be skipped when included later (windows.h chain).
  * -include this file so _MMINTRIN_H_INCLUDED is defined before any includes.
  */
-#ifndef _MMINTRIN_H_INCLUDED
-#define _MMINTRIN_H_INCLUDED
-#endif
+ #ifndef _MMINTRIN_H_INCLUDED
+ #define _MMINTRIN_H_INCLUDED
+ 
+ /* Provide a dummy __m64 type to satisfy downstream headers like xmmintrin.h */
+ typedef long long __m64;
+ 
+ #endif
